0e45549f70fb3f07dc6b962d66e91f56cfda4fed
Serhii Pysarev <pysarevsa@gmail.com>
2024-11-13 16:02:06 +0100

    rehrech

--- src/main/java/gmail/com/pysarevsa/Main.java
+++ src/main/java/gmail/com/pysarevsa/Main.java
@@ -2,26 +2,25 @@ package gmail.com.pysarevsa;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Scanner in = new Scanner(System.in);
         String hello = "Do you have a right hand?";
         String chus = "Y/N";
-        System.out.println(hello + "\n" + chus );
+        System.out.println(hello + "\n" + chus);
 
         String otvet = in.nextLine().toLowerCase();
 
-        if (otvet.equals("y")){
+        if (otvet.equals("y")) {
             System.out.println("Fuck");
             Thread.sleep(1000);
             System.out.println("you");
             Thread.sleep(1500);
             System.out.println("Picard!!!");
-        } else {
-            System.out.println("Luck");
+        } else if (otvet.equals("n")) {
+            System.out.println("Luck,");
             Thread.sleep(1500);
             System.out.println("I");
             Thread.sleep(1000);
@@ -30,9 +29,7 @@ public class Main {
             System.out.println("your");
             Thread.sleep(1000);
             System.out.println("FATHER!!!");
-        }
-
-
+        } else System.out.println("You are so stupid!!!");
 
 
     }
